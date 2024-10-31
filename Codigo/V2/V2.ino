0d7ad08db68368a98c693b93006ca29dace28185
EituCher18 <eitancherniak@gmail.com>
2024-10-31 14:21:23 -0300

    Fix compilation issues

--- Codigo/V2/V2.ino
+++ Codigo/V2/V2.ino
@@ -22,8 +22,6 @@ void setup() {
   // Set up led pin as outputs
   pinMode(led_pin, OUTPUT);
 
-  PS4.setLed(255, 0, 100);
-
   // Begin PS4 bluetooth connection
   PS4.begin("e0:5a:1b:d0:5c:2a");
 
@@ -33,17 +31,17 @@ void setup() {
 }
 
 template<typename T>
-T betterMap(T x, T in_min, T in_max, T out_min, T out_max) {
+T betterMap(const T x, const T in_min, const T in_max, const T out_min, const T out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 template<typename T, typename O>
-O linearInterpolation(T x, T* x_values, O* y_values, size_t len) {
+O linearInterpolation(const T x, const T* x_values, const O* y_values, size_t len) {
   size_t start_index = len - 2;
   size_t end_index = len - 1;
 
   for (size_t i = 0; i < len - 1; i++) {
-    if (input >= x_values[i]) {
+    if (x >= x_values[i]) {
       start_index = i;
       end_index = i;
     }
@@ -59,14 +57,14 @@ struct MotorSpeeds {
   bool right_forward;
 };
 
-MotorSpeeds calculateMotorSpeeds(int8_t x, int8_t y) {
-  constexpr float left_x_values = [0, PI/2, PI, 3*PI/2, 2*PI];
-  constexpr float left_y_values = [1, 1, 0, -1, 1];
-  constexpr float right_x_values = [0, PI/2, PI, 3*PI/2, 2*PI];
-  constexpr float right_y_values = [0, 1, 1, -1, 1];
+struct MotorSpeeds calculateMotorSpeeds(int8_t x, int8_t y) {
+  constexpr float left_x_values[] = { 0, PI / 2, PI, 3 * PI / 2, 2 * PI };
+  constexpr float left_y_values[] = { 1, 1, 0, -1, 1 };
+  constexpr float right_x_values[] = { 0, PI / 2, PI, 3 * PI / 2, 2 * PI };
+  constexpr float right_y_values[] = { 0, 1, 1, -1, 1 };
 
-  float angle = atan2(y, x);
-  float magnitude = constrain(sqrt(x * x + y * y) / 128.0, 0.0, 128.0);
+  float angle = atan2f(y, x);
+  float magnitude = constrain(sqrt(x * x + y * y), 0.0, 128.0) / 128.0;
 
   float left_speed = linearInterpolation(angle, left_x_values, left_y_values, sizeof(left_x_values) / sizeof(float));
   bool left_forward = true;
@@ -83,9 +81,9 @@ MotorSpeeds calculateMotorSpeeds(int8_t x, int8_t y) {
   }
 
   struct MotorSpeeds result = {
-    left_speed,
+    left_speed * magnitude,
     left_forward,
-    right_speed,
+    right_speed * magnitude,
     right_forward,
   };
   return result;
@@ -95,6 +93,8 @@ void loop() {
   if (PS4.isConnected() == false) {
     return;
   }
+
+  PS4.setLed(255, 0, 100);
 
   // Serial.println("Joystick PS4 conectado.");
 
@@ -114,7 +114,7 @@ void loop() {
     analogWrite(motor_left_forwards, 0);
     analogWrite(motor_left_backwards, motor_speeds.left_speed);
   }
-  
+
   if (motor_speeds.right_forward) {
     analogWrite(motor_right_forwards, motor_speeds.right_speed);
     analogWrite(motor_right_backwards, 0);
@@ -122,4 +122,6 @@ void loop() {
     analogWrite(motor_right_forwards, 0);
     analogWrite(motor_right_backwards, motor_speeds.right_speed);
   }
+
+  Serial.printf("%d\t%d\t%d\t%d\n", motor_speeds.left_speed, motor_speeds.left_forward, motor_speeds.right_speed, motor_speeds.right_forward);
 }
